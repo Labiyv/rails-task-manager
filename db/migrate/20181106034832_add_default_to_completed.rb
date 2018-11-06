ecaf124e4f77da3022374a5c724a9199156ee02d
yves labidoire <yves_labidoire@yahoo.com>
2018-11-06 17:30:28 +0900

    hell raised

---
+++ db/migrate/20181106034832_add_default_to_completed.rb
@@ -0,0 +1,12 @@
+class AddDefaultToCompleted < ActiveRecord::Migration[5.2]
+
+def up
+  change_column_default :tasks, :completed, false
+end
+
+def down
+  change_column_default :tasks, :completed, nil
+end
+
+
+end
